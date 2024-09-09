2d6e86b4c9e1ec4d7d1f9afce1c393005736b93e
Gautam Kumar <kumargautamsp@gmail.com>
2024-09-09 12:45:29 +0530

    Add CalendarIcon component for date display

---
+++ src/components/CalendarIcon.tsx
@@ -0,0 +1,20 @@
+const CalendarIcon = () => (
+    <svg
+      width="23px"
+      height="23px"
+      viewBox="0 0 24.00 24.00"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M20 10V7C20 5.89543 19.1046 5 18 5H6C4.89543 5 4 5.89543 4 7V10M20 10V19C20 20.1046 19.1046 21 18 21H6C4.89543 21 4 20.1046 4 19V10M20 10H4M8 3V7M16 3C16 3 16 5.4379 16 7"
+        stroke="#828282"
+        strokeWidth="1.08"
+        strokeLinecap="round"
+      />
+      <rect x="6" y="12" width="5" height="5" rx="1" fill="#828282" />
+    </svg>
+  );
+  
+  export default CalendarIcon;
+  
